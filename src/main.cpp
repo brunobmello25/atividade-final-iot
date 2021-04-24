8dec532a2a1c9367e963d5505cc3dc620f7ece7d
Bruno Mello <bruno.barros.mello@gmail.com>
2021-04-23 21:36:38 -0300

    feat: turn on the lights if dark

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@
 LedController ledController(RED_PIN, GREEN_PIN, BLUE_PIN);
 LightSensorController lightSensorController(LIGHT_SENSOR_PIN);
 
+boolean lightsOn = false;
+
 void setup()
 {
   Serial.begin(115200);
@@ -19,9 +21,12 @@ void loop()
 {
   ledController.loop();
   lightSensorController.loop();
+  ledController.updateColor(120, 0, 120);
 
-  ledController.updateColor(0, 0, 0);
+  int currentLight = lightSensorController.getLight();
 
-  Serial.println(lightSensorController.getLight());
+  Serial.println(currentLight);
+  ledController.toggle(currentLight < 400);
+
   delay(500);
 }
